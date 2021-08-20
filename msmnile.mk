f66372c0dd239b4bc3106a2a90a91965752800f2
nebrassy <nebras30@gmail.com>
2021-08-20 11:29:58 +0200

    AICPify, add AICP overlays
    Change-Id: I78d38c07f340212e610ac89738a1aec9c53c3333

--- msmnile.mk
+++ msmnile.mk
@@ -12,7 +12,8 @@ endif
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-aicp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
